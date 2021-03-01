d2c39eb7da0b6df31972119fcfa98954d72fdaa8
duy phương <79742795+duyphuong0211@users.noreply.github.com>
2021-03-01 16:03:59 +0700

    layout 2
    layout 2 , use 3 events, 1 to play, 1 to pause and 1 to exit

---
+++ Form2.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Media
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.lblMusicName = new System.Windows.Forms.Label();
+            this.pictureClose = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPlay = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPause = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPause)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMusicName
+            // 
+            this.lblMusicName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblMusicName.AutoSize = true;
+            this.lblMusicName.Location = new System.Drawing.Point(93, 492);
+            this.lblMusicName.Name = "lblMusicName";
+            this.lblMusicName.Size = new System.Drawing.Size(85, 17);
+            this.lblMusicName.TabIndex = 5;
+            this.lblMusicName.Text = "Music Name";
+            // 
+            // pictureClose
+            // 
+            this.pictureClose.BackColor = System.Drawing.Color.Maroon;
+            this.pictureClose.Image = ((System.Drawing.Image)(resources.GetObject("pictureClose.Image")));
+            this.pictureClose.Location = new System.Drawing.Point(12, 12);
+            this.pictureClose.Name = "pictureClose";
+            this.pictureClose.Size = new System.Drawing.Size(67, 68);
+            this.pictureClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureClose.TabIndex = 6;
+            this.pictureClose.TabStop = false;
+            this.pictureClose.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBoxPlay
+            // 
+            this.pictureBoxPlay.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBoxPlay.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxPlay.BackgroundImage")));
+            this.pictureBoxPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxPlay.Location = new System.Drawing.Point(622, 148);
+            this.pictureBoxPlay.Name = "pictureBoxPlay";
+            this.pictureBoxPlay.Size = new System.Drawing.Size(48, 52);
+            this.pictureBoxPlay.TabIndex = 7;
+            this.pictureBoxPlay.TabStop = false;
+            this.pictureBoxPlay.Click += new System.EventHandler(this.PictureBoxPlay_Click);
+            // 
+            // pictureBoxPause
+            // 
+            this.pictureBoxPause.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBoxPause.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxPause.BackgroundImage")));
+            this.pictureBoxPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxPause.Location = new System.Drawing.Point(622, 218);
+            this.pictureBoxPause.Name = "pictureBoxPause";
+            this.pictureBoxPause.Size = new System.Drawing.Size(48, 46);
+            this.pictureBoxPause.TabIndex = 8;
+            this.pictureBoxPause.TabStop = false;
+            this.pictureBoxPause.Click += new System.EventHandler(this.PictureBoxPause_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(96, 55);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(490, 419);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(682, 553);
+            this.Controls.Add(this.pictureBoxPause);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBoxPlay);
+            this.Controls.Add(this.pictureClose);
+            this.Controls.Add(this.lblMusicName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPause)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMusicName;
+        private System.Windows.Forms.PictureBox pictureClose;
+        private System.Windows.Forms.PictureBox pictureBoxPlay;
+        private System.Windows.Forms.PictureBox pictureBoxPause;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Media
+{
+    public partial class Form2 : Form
+    {
+        Music music;
+        System.Media.SoundPlayer player;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(Music music)
+        {
+            InitializeComponent();
+            this.music = music;
+            lblMusicName.Text = music.NameM1 + " - " + music.NameS1.Name;
+            player = new System.Media.SoundPlayer(music.Link);
+        }
+
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PictureBoxPlay_Click(object sender, EventArgs e)
+        {
+            player.Play();
+        }
+
+        private void PictureBoxPause_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
